117670940836031dda545dac82866f4cce60a338
Nicolás A <nlacosta@uc.cl>
2017-04-11 23:48:19 -0300

    Funcionalidad básica

--- CuboLED/CuboLED.ino
+++ CuboLED/CuboLED.ino
@@ -45,36 +45,69 @@ const byte latchOrder[] = {0,1,2,3,4,5,6,7};
 byte frameBuffer[8][64]; //[rows][cols]  . Llamado  x[col][row]
 byte bufferPointer;
 int fps = 60;
-unsigned long latchDelay = 250;
-void frameWrite(byte frame[], unsigned long layerDelay)
+unsigned long latchDelay = 1;
+
+byte frame[8][8] = {  {255,  255,  15,  15,  15,  15,  255,  255}, 
+                               {0,  0,  0,  0,  0,  0,  0,  0}, 
+                               {0,  0,  0,  0,  0,  0,  0,  0},   
+                               {0,  0,  0,  0,  0,  0,  0,  0},   
+                               {0,  0,  0,  0,  0,  0,  0,  0},   
+                               {0,  0,  0,  0,  0,  0,  0,  0},   
+                               {0,  0,  0,  0,  0,  0,  0,  0},  
+                               {0,  0,  0,  0,  0,  0,  0,  0} };
+void frameWrite(unsigned long layerDelay)
 {
   //Vamos pasando por capas
-  
   for(byte layer = 0; layer < 8; layer++)
   {
+    byte dataArr[] = {1,2,4,8,16,32,64,128};
     byte lSelect = layOrder[layer];  //Selector de capa
     PORTD = (PORTD & B11100011) | lSelect << 2;
-    Serial.println(lSelect,BIN);
-    Serial.println(lSelect << 2,BIN);
-    Serial.println(PORTD,BIN);
-    // B 8 7 6 5 4 3 2 1 0
-    byte addr = 1;
+//    Serial.println("layerdata");
+//    Serial.println(lSelect,BIN);
+//    Serial.println(lSelect << 2,BIN);
+//    Serial.println(PORTD,BIN);
+//    B 8 7 6 5 4 3 2 1 0
+    byte addr = 0;
     PORTD = (PORTD & B00011111);          //Selector de latch, se pone en 0 al inicio
-    for(int pointer = layer*8; pointer < 8*layer+8; pointer++) //Selector de dato
+    for(int addr = 0; addr<8;addr++)
     {
-      if(addr == 7)
-      {
-        addr = 0;
-        delayMicroseconds(latchDelay);
-      }
-      byte data = frame[pointer];
-      PORTB = (PORTB & B11000000) | (data & B00111111); //Carga data
-      PORTC = (PORTC & B11111100) | ((data & B11000000)>>6);   //Carga data
-      delayMicroseconds(latchDelay);
+      byte zero = 0;
+      PORTB = (PORTB & B11000000) |  (zero & B00111111); //Carga data
+      PORTC = (PORTC & B11111100) | ((zero & B11000000)>>6);   //Carga data
       PORTD = (PORTD & B00011111) | addr<<5; //Preparamos el siguiente latch y cargamos el dato del actual
-      addr++;
     }
-    delayMicroseconds(layerDelay);
+    PORTD = (PORTD & B00011111); 
+    Serial.println("++++++++++++++++++++++ \n LAYER");
+    for(int pointer = 1; pointer < 9; pointer++)
+    //for(int pointer = 8*layer; pointer < (8*layer)+8; pointer++) //Selector de dato
+    {
+      if(pointer == 9) pointer = 0;
+      byte data = frame[lSelect][pointer-1];
+      //Serial.println("-");
+      //Serial.println(lSelect,BIN);
+      //Serial.println(data,BIN);
+      //Serial.println(PORTB,BIN);
+      //Serial.println(PORTC,BIN);
+      //Serial.println("+");
+      PORTB = ((PORTB & B11000000) |  (data & B00111111)); //Carga data
+      PORTC = (PORTC & B11111100) | ((data & B11000000)>>6);   //Carga data
+      //Serial.println(PORTB,BIN);
+      //Serial.println(PORTC,BIN);
+      //Serial.println("-");
+      PORTD = (PORTD & B00011111) | pointer<<5; //Preparamos el siguiente latch y cargamos el dato del actual
+      delayMicroseconds(16);
+      //Serial.print("Byte n ");
+      //Serial.println(pointer);
+      //Serial.println(compose);
+      //Serial.println((data & B11000000)>>6);      
+      //Serial.println(data);
+      //Serial.println(PORTB,BIN);
+      //Serial.println(PORTC,BIN);
+      //Serial.println(PORTD,BIN);
+      
+    }
+    delay(1);
   }
 }
 
@@ -113,17 +146,47 @@ void setup()
   DDRD |= B11111100;
   Serial.println(F("READY"));
 }
+long del = 10000;
+
 void loop() 
 {                //linea 
-  byte frame[] = { 255, 0,0,0,0,0,0,0,
-                   255, 0,0,0,0,0,0,0,
-                   3, 0,0,0,0,0,0,0,
-                   3, 0,0,0,0,0,0,0,
-                   3, 0,0,0,0,0,0,0,
-                   3, 0,0,0,0,0,0,0,
-                   255, 0,0,0,0,0,0,0,
-                   255, 0,0,0,0,0,0,0};
+
+  /*byte frame[] = {
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255
+  };*/
+  /*  byte frame[] = { 0,255,255,255,255,255,255,255, 
+                     255,255,255,255,255,255,255,255, 
+                     0,  0,  0,  0,  0,  0,  0,  0,   
+                     0,  0,  0,  0,  0,  0,  0,  0,   
+                     0,  0,  0,  0,  0,  0,  0,  0,   
+                     0,  0,  0,  0,  0,  0,  0,  0,   
+                     255,255,255,255,255,255,255,255, 
+                     255,255,255,255,255,255,255,255, };*/
   unsigned long tprev = 0;
+  byte it = 0;
+  byte pttrnA[] = { B11100000
+                   ,B11100000
+                   ,B11100000
+                   ,B11100000
+                   ,B11100000
+                   ,B11100000
+                   ,B11111111
+                   ,B11111111};
+  byte pttrnB[] = { B11111111
+                   ,B11000011  
+                   ,B11000011
+                   ,B11111111
+                   ,B11111000
+                   ,B11001100
+                   ,B11000110
+                   ,B11000011};
   while(mode==0)
   {
     /*
@@ -131,12 +194,24 @@ void loop()
      * Al manejarlo vía hardware, podrían haber discrepancias de timing, en particular porque 60 fps no divide exacto el segundo en partes enteras, haciendo complicado manejar via delays, incluso NOPs
      * Provisionalmente se manejará en modo stream
      */
-    if(Serial.available()>=63) //recibo cuadro
+    if(millis() - tprev > 1000)
     {
-      Serial.print('k');
-      Serial.readBytes(frame, 64);
+      byte next = it+1;
+      if(next == 8) next = 0;
+      for(int i = 0; i < 8; i++)
+      {
+        if(it%2) frame[next][i] = pttrnA[i];
+        else frame[next][i] = pttrnB[i];
+      }
+      for(int i = 0; i < 8; i++)
+      {
+        frame[it][i] = 0;
+      }
+      tprev = millis();
+      it++;
+      if(it == 8) it = 0;
     }
-    
-    frameWrite(frame,250); //Escribo cuadro... Esta rutina los escribe lo mas rápido posible
+    delay(5);
+    frameWrite(del); //Escribo cuadro... Esta rutina los escribe lo mas rápido posible
   }
 }
